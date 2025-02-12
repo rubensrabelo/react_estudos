4211ff4277a2bae7102a816cc4122f9400392ddb
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-12 15:42:07 -0300

    Doing the project

--- 06_section_secret_word/src/App.jsx
+++ 06_section_secret_word/src/App.jsx
@@ -5,6 +5,8 @@ import { useCallback, useEffect, useState } from 'react';
 import { wordsList } from './data/words';
 
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
 
 const stages = [
   { id: 1, name: "start" },
@@ -14,14 +16,26 @@ const stages = [
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
-  console.log(wordsList);
+  const [words] = useState(wordsList);
+
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  };
+
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
 
   return (
     <>
       <div className="App">
-      {gameStage === "start" && <StartScreen />};
-      {gameStage === "game" && <StartScreen />};
-      {gameStage === "end" && <StartScreen />};
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
       </div>
     </>
   );
