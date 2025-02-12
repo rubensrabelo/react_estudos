241cec06c6a7ee9f9c2b13dd6d59f00a00d40d50
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-12 09:34:45 -0300

    Adding Game component

--- 06_section_secret_word/src/App.jsx
+++ 06_section_secret_word/src/App.jsx
@@ -1,16 +1,30 @@
-import './App.css'
+import './App.css';
 
-import StartScreen from './components/StartScreen'
+import { useCallback, useEffect, useState } from 'react';
+
+import { wordsList } from './data/words';
+
+import StartScreen from './components/StartScreen';
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" }
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  console.log(wordsList);
 
   return (
     <>
       <div className="App">
-        <StartScreen />
+      {gameStage === "start" && <StartScreen />};
+      {gameStage === "game" && <StartScreen />};
+      {gameStage === "end" && <StartScreen />};
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
