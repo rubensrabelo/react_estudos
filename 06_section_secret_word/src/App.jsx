e8a77e2922c5d44147f6f3ccc5e3ff48b0109fcb
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-13 13:15:20 -0300

    Fixing code

--- 06_section_secret_word/src/App.jsx
+++ 06_section_secret_word/src/App.jsx
@@ -18,7 +18,18 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.floor(Math.random() * Object.keys(categories).length))];
+  };
+
   const startGame = () => {
+    pickWordAndCategory();
+    
     setGameStage(stages[1].name);
   };
 
