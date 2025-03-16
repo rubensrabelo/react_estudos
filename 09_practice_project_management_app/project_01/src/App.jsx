714c5a07e4ac9632f6fc3087cdcf8af98e1f076e
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-16 15:37:45 -0300

    Continuing study section 09 part 2

--- 09_practice_project_management_app/project_01/src/App.jsx
+++ 09_practice_project_management_app/project_01/src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSideBar from "./components/ProjectsSideBar.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProject: null
+      };
+    });
+  }
+
+  let content;
+
+  if(projectsState.selectedProject === null) {
+    content = <NewProject />;
+  } else if(projectsState.selectedProject === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+  
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar />
-      <NewProject />
+      <ProjectsSideBar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
