0362c91eb5394195cc1f435e24f00576dc7f87fa
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-16 18:59:35 -0300

    Continuing study section 09 part 3

--- 09_practice_project_management_app/project_01/src/App.jsx
+++ 09_practice_project_management_app/project_01/src/App.jsx
@@ -18,17 +18,32 @@ function App() {
     });
   }
 
+  function handleAddProject(projectData) {
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject]
+      };
+    });
+  }
+
   let content;
 
   if(projectsState.selectedProject === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handleAddProject} />;
   } else if(projectsState.selectedProject === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
   
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar onStartAddProject={handleStartAddProject} />
+      <ProjectsSideBar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
       {content}
     </main>
   );
